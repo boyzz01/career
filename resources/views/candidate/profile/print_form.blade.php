93505c18f64fc28520252616f01f751e7e93515a
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-11-23 12:21:31 +0700

    a

--- resources/views/candidate/profile/print_form.blade.php
+++ resources/views/candidate/profile/print_form.blade.php
@@ -6,9 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       
         {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" /> --}}
-        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/third-party.css') }}">
+        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/third-party.css') }}">--}}
       
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">--}}
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/plugins.css') }}"> 
       
         <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/custom.css') }}">
